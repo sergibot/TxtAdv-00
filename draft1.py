ada14d8b3abbfc16ce03b4c46e8916c51d3fd4d8
sergibot <sergi.bray@gmail.com>
2018-08-29 20:46:51 +0100

    Drafting what needs doing, adding monsters file (1st draft: monsters like hunger, FOMO...)

---
+++ draft1.py
@@ -0,0 +1,17 @@
+'''
+
+Draft of game plan:
+intro pic + music! press enter to start.
+different music? (each room? annoying?)
+intro w field and door.
+loc: lib (narr might be a book?)
+look -> tell player location, poss interactions
+mememe -> tell health, inventory, ...?
+other rooms: cafe; bar; library; park.
+enemies: 1hunger, 2FOMO, 3bees, 4awareness of teeth/eyes(upsddwnvsn)/clothes.
+
+'''
+
+
+
+
